0ce9e06cc1eab28415c4cd444dc6a6acc6b3e6ca
Eran Keydar <ekeydar@gmail.com>
2018-01-12 22:30:01 +0200

    wip

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener(
         //alert("onMessage request = " + request);
         if (request.print) {
             console.log(request, sender);
-            chrome.tabs.query({active:true}, function(tabs) {
+            chrome.tabs.query({active: true}, function (tabs) {
                 console.log("after query tabs = ", tabs);
                 if (tabs.length == 1) {
                     printTab(tabs[0]);
@@ -17,8 +17,8 @@ chrome.runtime.onMessage.addListener(
     });
 
 function log(tab, msg) {
-    var msg = msg.replace(/[']/g,'"');
-    chrome.tabs.executeScript(tab.id,{
+    var msg = msg.replace(/[']/g, '"');
+    chrome.tabs.executeScript(tab.id, {
         code: "console.log('" + msg + "')"
     });
 }
@@ -28,18 +28,33 @@ function printTab(tab) {
     log(tab, "start printing...");
     chrome.tabs.captureVisibleTab(null, {
         format: 'png'
-    }, function(data) {
-        printDone(tab, data);
+    }, function (data) {
+        chrome.tabs.executeScript(tab.id, {'file': 'sizes.js'},
+            function (results) {
+                let sizes = JSON.parse(results[0]);
+                cropImage(data, {x: x, y: y, w: w, h: h}, function (croppedData) {
+                    printDone(tab, croppedData)
+                })
+            })
     });
+}
+
+function cropImage(data, sizes, callback) {
+    callback(data);
 }
 
 function printDone(tab, data) {
     log(tab, "printDone");
     log(tab, "length = " + data.length);
+    log(tab, data.substr(0, 100));
     chrome.tabs.create({
-        url: 'results.html'
-    }, function(resultTab) {
-        console.log(resultTab);
+        url: 'http://localhost:9005/results.html'
+    }, function (resultTab) {
+        log(resultTab, "going to pupulate image");
+        chrome.tabs.executeScript(resultTab.id,
+            {
+                'code': 'var elem = document.getElementById("img-main") ; console.log(elem); elem.src="' + data + '"'
+            });
     })
 }
 
---
+++ sizes.js
@@ -0,0 +1,9 @@
+var elem =document.getElementById("mapcontainer");
+console.log(elem);
+var rect = elem.getBoundingClientRect();
+console.log(rect);
+JSON.stringify(rect);
+
+
+
+
